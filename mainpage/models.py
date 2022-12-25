a0fcdf7e7b8f2bb818081e7b73a7e8aefbb79889
Anastasia <nastena.maiorova@gmail.com>
2022-12-25 16:38:37 +0300

    Добавили в админку поля для ввода сотрудников

--- mainpage/models.py
+++ mainpage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    position = models.CharField(default='', max_length=1024)
+    surname = models.CharField(default='', max_length=1024)
+    name = models.CharField(default='', max_length=1024)
+    fathername = models.CharField(default='', max_length=1024)
+    date_of_birth = models.DateTimeField()
+    phone = models.IntegerField()
+    email = models.CharField(default='', max_length=1024)
