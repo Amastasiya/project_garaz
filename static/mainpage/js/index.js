90c8f147c53df6bbde5cd47a112751ef3d16adf6
Anastasia <nastena.maiorova@gmail.com>
2022-12-28 20:53:45 +0300

    Вывели дату и время

--- static/mainpage/js/index.js
+++ static/mainpage/js/index.js
@@ -1,13 +1,16 @@
-let time = document.createElement("div");
+//let time = document.createElement("div");
 let date = new Date();
-time.innerHTML = `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`;
-document.getElementBuId("timer1").appendChild(time)
+/*time.innerHTML = `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`;
+document.getElementById("timer1").appendChild(time);
+*/
+let dtstr = `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`;
+document.getElementById("timer1").innerHTML = dtstr;
+console.log(dtstr);
 
-
-window.onload = function(){
+/*window.onload = function(){
   window.setInterval(function(){
   var now = new Date();
   var clock = document.getElementById("clock");
   clock.innerHTML = now.toLocaleTimeString();
   },1000);
-};
+};*/
