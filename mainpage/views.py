4a005ce4b3495fb5c8eec01ac130bdf1c3dc5e2b
Anastasia <nastena.maiorova@gmail.com>
2022-12-28 15:52:38 +0300

    Создать страницу с пунктами меню    Меню создаётся из списка строк    Использовать ветвление и циклы для наполнения страницы

--- mainpage/views.py
+++ mainpage/views.py
@@ -8,7 +8,15 @@ def index(request):
         'nomer_okoshka': '9 и 3/4',
         'name': 'Гараж', # добавили для index.html
         'employee': Employee.objects.all(),
-        'guide': Guide.objects.all(),}
+        'guide': Guide.objects.all(),
+        'menu_one': [
+            'События',
+            'Телефонный справочник',
+            'Календарь',
+            'История'
+        ]
+    }
+
     return render(
         request,               #Запрос
         'mainpage/index.html', # путь к шаблону
