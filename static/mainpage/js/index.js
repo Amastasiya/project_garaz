3034c4c34437e689fcd2afd4d18b3a100deb1666
Anastasia <nastena.maiorova@gmail.com>
2023-01-26 21:38:47 +0300

    Возвращаем запрос

--- static/mainpage/js/index.js
+++ static/mainpage/js/index.js
@@ -6,15 +6,38 @@ function insert_date() {
     */
     let dtstr = `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`;
     document.getElementById("timer1").innerHTML = dtstr;
-    console.log(dtstr);
+    //console.log(dtstr);
     setTimeout(insert_date, 500);
 }
 
 insert_date();
 
 function cry() {
-    console.log("search")
-    fetch()
+    console.log("search");
+    let user = {
+        name: 'Igor',// достать данные из базы!!!
+        phone: '444'
+    };
+    let tokens = document.getElementsByName('csrfmiddlewaretoken');
+    console.log(tokens[0].value);
+    fetch('/mainpage/', {
+        method: 'POST',
+		headers: {
+	        'Content-Type': 'application/json',
+			'X-CSRFToken': tokens[0].value,
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: 'Igor',// достать данные из базы!!!
+            phone: '444'
+        })
+    }).then(response => {
+			console.log(response);
+			return response.json();
+	}).then(result => {
+		console.log(result)
+	});
+
 
 }
 /*window.onload = function(){
